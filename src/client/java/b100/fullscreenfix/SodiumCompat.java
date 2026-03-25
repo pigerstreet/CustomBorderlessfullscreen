337cbf6ecdb2e152a51a0588952aa6c2f1cfd756
bestsoft100 <163331759+Bestsoft101@users.noreply.github.com>
2026-03-25 21:29:22 +0100

    Mod now works without sodium installed again

--- src/client/java/b100/fullscreenfix/SodiumCompat.java
+++ src/client/java/b100/fullscreenfix/SodiumCompat.java
@@ -16,8 +16,8 @@ public class SodiumCompat {
 	public static OptionBuilder getCustomFullscreenButton(ConfigBuilder builder) {
 		EnumOptionBuilder<FullscreenMode> optionBuilder = builder.createEnumOption(Identifier.fromNamespaceAndPath("sodium", "general.fullscreen_mode"), FullscreenMode.class);
 		
-		Consumer<FullscreenMode> set = value -> FullscreenFix.setFullscreenMode(value);
-		Supplier<FullscreenMode> get = FullscreenFix::getCurrentFullscreenMode;
+		Consumer<FullscreenMode> set = value -> FullscreenFix.setFullscreenMode(convert(value));
+		Supplier<FullscreenMode> get = () -> convert(FullscreenFix.getCurrentFullscreenMode());
 		StorageEventHandler save = FullscreenFix.CONFIG::save;
 		
 		optionBuilder.setName(FullscreenFix.TRANS.asText("option.fullscreen"));
@@ -36,5 +36,26 @@ public class SodiumCompat {
 		});
 		
 		return optionBuilder;
+	}
+	
+	// This is really ugly but the option in the sodium menu requires sodium's fullscreen mode,
+	// but when running the mod without sodium the enum is still needed
+	
+	public static b100.fullscreenfix.FullscreenMode convert(FullscreenMode mode) {
+		if(mode == FullscreenMode.BORDERLESS) {
+			return b100.fullscreenfix.FullscreenMode.BORDERLESS;
+		}else if(mode == FullscreenMode.EXCLUSIVE) {
+			return b100.fullscreenfix.FullscreenMode.EXCLUSIVE;
+		}
+		return b100.fullscreenfix.FullscreenMode.OFF;
+	}
+	
+	public static FullscreenMode convert(b100.fullscreenfix.FullscreenMode mode) {
+		if(mode == b100.fullscreenfix.FullscreenMode.BORDERLESS) {
+			return FullscreenMode.BORDERLESS;
+		}else if(mode == b100.fullscreenfix.FullscreenMode.EXCLUSIVE) {
+			return FullscreenMode.EXCLUSIVE;
+		}
+		return FullscreenMode.OFF;
 	}
 }
